d6d8eea80d72d6ea71684ac664852fef2f9629d1
xcoda <dltpdn@gmail.com>
2017-01-15 22:22:27 +0900

    Add camera capture to a file

--- camera_capture_file.py
+++ camera_capture_file.py
@@ -1,10 +1,18 @@
-import cv2, time
+import time
+import picamera
 
-cam = cv2.VideoCapture(0)
-
-while True:
-    ret,img = cam.read()
-    cv2.imshow('Camera', img)
-    key = cv2.waitKey(10)
-    if key == ord(' '):
-        time.sleep(1000)
+with picamera.PiCamera() as camera:
+    try:
+        camera.start_preview()
+        while True:
+            shutter = input('insert key when you are ready to take photo. [photo:1, video:2] ')
+            now_str = time.strftime("%Y%m%d-%H%M%S")
+            if shutter == 1:
+                camera.capture('/home/pi/photo%s.gif' %now_str)
+            elif shutter == 2:
+                camera.start_recording('/home/pi/video%s.h264' %now_str)
+                raw_input('insert key when you want to stop recoding.')
+                camera.stop_recording()
+    finally:
+        camera.stop_preview()
+        camera.close()
